cd3fc69c0aaa54006b886837c79a66422d63359f
Waut <sebastien.farese@gmail.com>
2021-01-07 17:02:54 +0100

    first commit

---
+++ menu/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Pizza
+
+
+# Create your views here.
+
+
+def index(request):
+    '''pizzas = Pizza.objects.all()
+    pizzas_name_and_price = [pizza.nom + " : " + str(pizza.prix) + " €" for pizza in pizzas]
+    pizzas_name_and_price_str = ", ".join(pizzas_name_and_price)
+
+    return HttpResponse("Les pizzas: "+ pizzas_name_and_price_str)'''
+    pizzas = Pizza.objects.all().order_by('prix')
+    return render(request, 'menu/index.html', {'pizzas': pizzas})
